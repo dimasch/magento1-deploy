efff7d71a1a019a809ca3a916bef040abbf8e6c1
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-08-01 00:25:01 +0400

    Fixed sleep period.

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 curl -O https://files.magerun.net/n98-magerun.phar
 chmod +x ./n98-magerun.phar
+sleep 5
 
 echo "Generate local.xml with use environment vars..."
 ./n98-magerun.phar local-config:generate -n $DB_HOST $DB_USER $DB_PASS $DB_NAME
-sleep 100
+sleep 5
 echo "Clean magento cache..."
 ./n98-magerun.phar cache:flush && ./n98-magerun.phar cache:clean
 
