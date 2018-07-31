fcf94f22bbfb4fa91b30c608a4f14243012aa3aa
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-08-01 00:14:37 +0400

    Generate local.xml and clean cache.

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+
+echo "Generate local.xml with use environment vars..."
+n98-magerun local-config:generate -n $DB_HOST $DB_USER $DB_PASS $DB_NAME
+echo "Clean magento cache..."
+n98-magerun cache:flush && n98-magerun cache:clean
+
