9aad9ceb9e3acddbd10aa01669a7bed745a2f31c
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-08-01 00:22:41 +0400

    Download n98 for AWS.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+curl -O https://files.magerun.net/n98-magerun.phar
+chmod +x ./n98-magerun.phar
+
 echo "Generate local.xml with use environment vars..."
-n98-magerun local-config:generate -n $DB_HOST $DB_USER $DB_PASS $DB_NAME
+./n98-magerun.phar local-config:generate -n $DB_HOST $DB_USER $DB_PASS $DB_NAME
 sleep 100
 echo "Clean magento cache..."
-n98-magerun cache:flush && n98-magerun cache:clean
+./n98-magerun.phar cache:flush && ./n98-magerun.phar cache:clean
 
