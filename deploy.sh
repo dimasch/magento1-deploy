09859baf9d1d5833d9cf20bbd10c41ef6c004d82
Dmitry Schegolihin <d.shegolihin@gmail.com>
2018-08-26 03:00:38 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,14 @@ curl -O https://files.magerun.net/n98-magerun.phar
 chmod +x ./n98-magerun.phar
 sleep 5
 
+echo "Install magento core..."
+composer install -vvv
+
 echo "Generate local.xml with use environment vars..."
 ./n98-magerun.phar local-config:generate -n $DB_HOST $DB_USER $DB_PASS $DB_NAME
+
 sleep 5
+
 echo "Clean magento cache..."
 ./n98-magerun.phar cache:flush && ./n98-magerun.phar cache:clean
 
